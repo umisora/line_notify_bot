a45da46aba191d9fd490ef1c4b73415909dea0e5
umisora <bit72s_akm@hotmail.com>
2017-05-13 18:15:15 +0900

    line echo bot

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 require 'line/bot'
 
+TARGET_IDS = []
 # 32文字
-get '/jftiajp6trq6bvvs79s9qqp6qgvr4z2a' do
+get '/' do
   "Hello world"
 end
 
@@ -13,13 +14,22 @@ def client
   }
 end
 
-post '/callback' do
-  body = request.body.read
+def push(message)
+  ENV['TARGET_IDS'].split(",").each{ |id|
+    client.push_message(id,message)
+    TARGET_IDS.push(line_id) 
+  }
+end
 
-  signature = request.env['HTTP_X_LINE_SIGNATURE']
-  unless client.validate_signature(body, signature)
-    error 400 do 'Bad Request' end
-  end
+post '/callback' do
+  referrer = request.referrer.to_s
+  useragent = request.user_agent.to_s
+  body = request.body.read
+  body_json = JSON.parse(body)
+  #signature = request.env['HTTP_X_LINE_SIGNATURE']
+  #unless client.validate_signature(body, signature)
+  #  error 400 do 'Bad Request' end
+  #end
 
   events = client.parse_events_from(body)
   events.each { |event|
@@ -27,10 +37,22 @@ post '/callback' do
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
-        message = {
-          type: 'text',
-          text: event.message['text']
-        }
+        if event.message['text'].include?('id')
+          message = {
+            type: 'text',
+            text: 'Your User ID is :' + body_json["events"][0]["source"]["userId"].to_s
+          }
+        elsif event.message['text'].include?('agent')
+          message = {
+            type: 'text',
+            text: "referrer : " + referrer + "|| useragent : " + useragent
+          }
+        else 
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+        end
         client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
